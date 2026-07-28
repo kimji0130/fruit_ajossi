97f94bc46ec477e3f8955398cb6a67f670ae66c0
kimji0130 <mujigekr@hanmail.net>
2026-07-28 18:00:38 +0900

    onChange (e) e.target.value 의미

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,8 +2,8 @@ function Search({search, setSearch}){
   return(
     <input 
     value = {search} placeholder="과일을 검색하세요."
-    onChange = {(e) => {
-      setSearch(e.target.value);
+    onChange = {(e) => { //onChange가 실행될 때 react가 "e"를 전달해주니까 꺼내 쓸수 있는거다..
+      setSearch(e.target.value);// e.target.value = 방금 입력한 값 (임시), search= react가 기억하고 있는 값(저장됨)
     }}
     />
     
